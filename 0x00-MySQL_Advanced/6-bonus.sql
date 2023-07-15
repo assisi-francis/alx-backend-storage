e7760691b64ae789a64d7ef7efa89eff2f245061
assisi-francis <doncyco123@gmail.com>
2023-07-15 21:58:14 +0100

    task 6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -3,26 +3,13 @@
 --user_id
 -- project_name
 -- score
-CREATE PROCEDURE AddBonus(
-  IN user_id INT,
-  IN project_name VARCHAR(255),
-  IN score DECIMAL(10,2)
-)
+DELIMITER $$;
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT )
 BEGIN
-  DECLARE project_id INT;
-
-  -- Check if the project already exists
-  SELECT id INTO project_id FROM projects WHERE name = project_name;
-
-  -- If the project doesn't exist, create it
-  IF project_id IS NULL THEN
-    INSERT INTO projects (name) VALUES (project_name);
-    SET project_id = LAST_INSERT_ID();
-  END IF;
-
-  -- Add the correction for the student
-  INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
-
-  -- Optional: You can include additional logic or actions here if needed
-
-END;
+	IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
+		INSERT INTO projects (name) VALUES (project_name);
+	END IF;
+	INSERT INTO corrections (user_id, project_id, score)
+	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
+END;$$
+DELIMITER ;
