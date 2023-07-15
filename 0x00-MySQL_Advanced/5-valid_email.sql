1928c84f1e4333e2a44dff4e349e01e1fae37289
assisi-francis <doncyco123@gmail.com>
2023-07-15 21:42:35 +0100

    modify task 5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,5 +1,6 @@
 -- a SQL script that creates a trigger that resets valid_email
 -- only when email is been changed
+DELIMITER $$ ;
 CREATE TRIGGER reset_valid_email_trigger BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
@@ -7,3 +8,4 @@ BEGIN
         SET NEW.valid_email = 0;
     END IF;
 END;
+DELIMITER ;
