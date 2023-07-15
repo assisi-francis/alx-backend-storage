fc226747c6caa98168f70a3b06ae4985c395d572
assisi-francis <doncyco123@gmail.com>
2023-07-15 20:55:30 +0100

    modify task 4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -5,6 +5,6 @@ CREATE TRIGGER decrease_quantity_trigger AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - NEW.quantity
-    WHERE item_id = NEW.item_id;
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
 END;
