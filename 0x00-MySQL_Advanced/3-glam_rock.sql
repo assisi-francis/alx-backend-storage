a13007ffc40dfa7a41593535226261ead41d5c76
assisi-francis <doncyco123@gmail.com>
2023-07-15 20:32:27 +0100

    task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 -- ranked by their loongevity
 -- column name: band_name and lifespan (using 2022 instead of YEAR(CURDATED())
 -- Using attributes formed and split for computing lifespan
-SELECT band_name, (YEAR('2022-01-01') - YEAR(formed)) AS lifespan
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
     FROM metal_bands
     WHERE style LIKE '%Glam rock%'
     ORDER BY lifespan DESC;
