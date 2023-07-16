2ce0878a7de85ea228c047a9994df2dda53d002a
assisi-francis <doncyco123@gmail.com>
2023-07-16 07:54:43 +0100

    task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -10,16 +10,31 @@ CREATE PROCEDURE ComputeAverageScoreForUser(
 )
 BEGIN
   DECLARE avg_score DECIMAL(10,2);
-  
-  -- Calculate the average score for the user
-  SELECT AVG(score) INTO avg_score
+  DECLARE project_count INT;
+
+  -- Count the number of projects for the user
+  SELECT COUNT(DISTINCT project_id) INTO project_count
   FROM corrections
   WHERE user_id = user_id;
+
+  -- Calculate the average score for the user
+  IF project_count >= 1 THEN
+    SELECT AVG(score) INTO avg_score
+    FROM (
+      SELECT score
+      FROM corrections
+      WHERE user_id = user_id
+      ORDER BY score DESC
+      LIMIT project_count
+    ) AS subquery;
+  ELSE
+    SET avg_score = 0;
+  END IF;
 
   -- Update the average score for the user in the users table
   UPDATE users
   SET average_score = avg_score
   WHERE id = user_id;
-  
+
 END$$
 DELIMITER ;
