24ac984f7efc578c061345f0b43e68ae0c00dcf0
assisi-francis <doncyco123@gmail.com>
2023-07-16 08:58:36 +0100

    task 10

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -2,9 +2,9 @@
 -- The function SafeDiv takes 2 arguments: a, INT b, INT
 -- And returns a / b or 0 if b == 0
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS INT
+RETURNS FLOAT
 BEGIN
-  DECLARE result INT;
+  DECLARE result FLOAT;
   
   IF b = 0 THEN
     SET result = 0;
